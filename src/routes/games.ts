2913e46d9d06918f2961fb7cb0887efe2b1639f9
David Scheuermann <dsmann1212@gmail.com>
2020-03-28 01:18:59 -0500

    Updated games route to connect to database

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,14 +1,59 @@
 import express from 'express';
 import Game from '../models/game';
+import Company from '../models/company';
+import Platform from '../models/platform';
+import Config from '../config/config';
+import apicalypse from 'apicalypse';
+import pg from 'pg';
 
 const router: express.Router = express.Router();
+const config = new Config();
 
-router.get('/', (req, res) => {
-    let game = new Game(1, "Shadow of the Colossus", "A boy will fight giants for the woman he loves");
+const database: pg.Client = new pg.Client({
+    host: config.database_host,
+    user: config.database_user,
+    password: config.database_pw,
+    database: config.database_db,
+});
 
-    let html: string = `<h1>${game.name}</h1>` +
-        `<p>${game.summary}</p>`;
-    res.send(html);
+router.get('/', async (req, res) => {
+    const requestOptions = {
+        // queryMethod: 'url',
+        queryMethod: 'body',
+        baseURL: 'https://api-v3.igdb.com',
+        headers: {
+            accept : 'application/json',
+            'user-key': config.igdb_api_key,
+        },
+        responseType: 'json',
+        timeout: 10000, // 1 second timeout
+    };
+
+    try {
+        // let query = req.query;
+        database.connect();
+        let query_response = await database
+            .query("SELECT game.*, developers.company_id AS developer_id, \
+                    developers.company_name AS developer_name, \
+                    developers.description AS developer_description, \
+                    developers.country AS developer_country, \
+                    publishers.company_id AS publisher_id, \
+                    publishers.company_name AS publisher_name, \
+                    publishers.description AS publisher_description, \
+                    publishers.country AS publisher_country FROM game \
+                    INNER JOIN company developers \
+                    ON game.developer = developers.company_id \
+                    INNER JOIN company publishers \
+                    ON game.publisher = publishers.company_id" );
+
+        // res.send(response.data);
+        res.send(query_response.rows);
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    } finally {
+        database.end();
+    }
 });
 
 export default router;
